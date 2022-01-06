283f9bef9f59237b0d68ae510eb5a316ebffbe8d
unknown <golden.star0228@gmail.com>
2022-01-06 09:25:23 -0500

    removed dot notaion

--- src/stories/WhyExist.tsx
+++ src/stories/WhyExist.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
-import styled from "styled-components"
 import ItemNumber from './ItemNumber'
-import * as Components from './Components'
+import {
+  ComponentContainer,
+  ComponentHeader,
+  ComponentComment,
+  ProblemSolution,
+  TableLogo,
+  TableHeader,
+  TableItem,
+  TableItemHeader,
+  TableItemUl,
+  Problem,
+  Solution,
+} from './Components'
 
 import './styles/main.scss'
 
@@ -9,9 +20,9 @@ interface PropsType {}
 
 export const WhyExist = (props: PropsType) => {
   return (
-    <Components.ComponentContainer>
-      <Components.ComponentHeader>Why we exist</Components.ComponentHeader>
-      <Components.ComponentComment>
+    <ComponentContainer>
+      <ComponentHeader>Why we exist</ComponentHeader>
+      <ComponentComment>
         It's hard to find talented engineers.
         Whether you're a ting startup, or a huge corporation.
         Whether you can afford $60 salary or $300k, there will always be a startage good engineers.
@@ -24,70 +35,70 @@ export const WhyExist = (props: PropsType) => {
         Dilli, our CLO, comes from a legal background. He's been working with immigration for 10 years and system like the book of this hand.
         <br/>
         We indentifiled some problems in the industries and how combining our unique talents can solve them.
-      </Components.ComponentComment>
-      <Components.ProblemSolution>
-        <Components.TableLogo>
+      </ComponentComment>
+      <ProblemSolution>
+        <TableLogo>
           <img src="/problem_solution_icon.png" alt="problem-solution" />
-        </Components.TableLogo>
-        <Components.TableHeader>
+        </TableLogo>
+        <TableHeader>
           <div>Problem</div>
           <div>Solution</div>
-        </Components.TableHeader>
-        <Components.TableItem>
-          <Components.Problem>
-            <Components.TableItemHeader>Finding Good Engineers is Hard:</Components.TableItemHeader>
+        </TableHeader>
+        <TableItem>
+          <Problem>
+            <TableItemHeader>Finding Good Engineers is Hard:</TableItemHeader>
             <div className='content'>
               you used staffing companies, recriting companies, offer employees referal bounses. You offer 1.5x Google Salaries. But still you can't find employees. We've been there!
             </div>
             <ItemNumber number={1} />
-          </Components.Problem>
-          <Components.Solution>
-            <Components.TableItemHeader>We're Developers Ourselves, and are Constantly Hanging Around Other Devs, Such as at:</Components.TableItemHeader>
+          </Problem>
+          <Solution>
+            <TableItemHeader>We're Developers Ourselves, and are Constantly Hanging Around Other Devs, Such as at:</TableItemHeader>
             <div className='content'>
-              <Components.TableItemUl>
+              <TableItemUl>
                 <li>Hackathons</li>
                 <li>Conferences</li>
                 <li>Tech events/Meetup Groups</li>
-              </Components.TableItemUl>
+              </TableItemUl>
               Also a lot of our engineers come from oversees
             </div>
-          </Components.Solution>
-        </Components.TableItem>
-        <Components.TableItem>
-          <Components.Problem>
-            <Components.TableItemHeader>Interviewing Takes Forever:</Components.TableItemHeader>
+          </Solution>
+        </TableItem>
+        <TableItem>
+          <Problem>
+            <TableItemHeader>Interviewing Takes Forever:</TableItemHeader>
             <div className='content'>
               I worked for a company that was offering amazing salaries benefits and stock optios. We had a team of 8 engineers, and were ambitious to scale up. But all the condates sent to us were not qualified.
             </div>
             <ItemNumber number={2} />
-          </Components.Problem>
-          <Components.Solution>
+          </Problem>
+          <Solution>
             We don't waste your time with junior and mid-level developers.
             <br/>
             We ONLY recruit the top 5%. Our intensive 6 hour interview process ensures that our employees are yop notch. We also work directly with these developers for a month on our internal consulting projects.
-          </Components.Solution>
-        </Components.TableItem>
-        <Components.TableItem>
-          <Components.Problem>
-            <Components.TableItemHeader>Finding Good Engineers is Hard:</Components.TableItemHeader>
+          </Solution>
+        </TableItem>
+        <TableItem>
+          <Problem>
+            <TableItemHeader>Finding Good Engineers is Hard:</TableItemHeader>
             <div className='content'>
               you used staffing companies, recriting companies, offer employees referal bounses. You offer 1.5x Google Salaries. But still you can't find employees. We've been there!
             </div>
             <ItemNumber number={3} />
-          </Components.Problem>
-          <Components.Solution>
-            <Components.TableItemHeader>We're Developers Ourselves, and are Constantly Hanging Around Other Devs, Such as at:</Components.TableItemHeader>
+          </Problem>
+          <Solution>
+            <TableItemHeader>We're Developers Ourselves, and are Constantly Hanging Around Other Devs, Such as at:</TableItemHeader>
             <div className='content'>
-              <Components.TableItemUl>
+              <TableItemUl>
                 <li>Hackathons</li>
                 <li>Conferences</li>
                 <li>Tech events/Meetup Groups</li>
-              </Components.TableItemUl>
+              </TableItemUl>
               Also a lot of our engineers come from oversees
             </div>
-          </Components.Solution>
-        </Components.TableItem>
-      </Components.ProblemSolution>
-    </Components.ComponentContainer>
+          </Solution>
+        </TableItem>
+      </ProblemSolution>
+    </ComponentContainer>
   )
 }
